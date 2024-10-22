ece0d277ff9c673c363a95a1a27e584b86586b60
Hikkos <noeparedes027@hotmail.com>
2024-10-22 12:51:15 -0600

    added server

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "http://localhost:3000/api";
+const baseURL = "https://pwa-back-production.up.railway.app/api/todos";
 const axiosInstance = axios.create({
   baseURL: baseURL,
   headers: {
